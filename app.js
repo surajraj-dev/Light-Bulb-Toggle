e658c5e7925ed26c1301203d649e51941f5b2d58
Suraj Raj <srjdev2003@gmail.com>
2025-02-25 14:51:51 +0530

    I focusing to adding functionality using JavaScript.

---
+++ app.js
@@ -0,0 +1,29 @@
+/**
+ * Write your challenge solution here
+ */
+
+const toggleBtn = document.getElementById('toggleButton')
+// console.log(toggleBtn.innerText)
+const bulb = document.getElementById('bulb')
+// console.log(bulb);
+const bulbStatus = document.getElementById('status')
+
+
+function bulbFunctionality(){
+    if(bulbStatus.innerText === 'Status: Off'){
+        // it means bulb glow
+
+        //this line basically define we have add .off class properties inside the bulb dinamically
+        bulb.classList.remove('off')   
+        toggleBtn.innerText='Turn Off';
+        bulbStatus.innerText = 'Status: On'
+
+    } else{
+        // it means bulb not glow
+        bulb.classList.add('off')
+        toggleBtn.innerText='Turn On';  
+        bulbStatus.innerText = 'Status: Off';
+    }
+
+}
+toggleBtn.addEventListener('click', bulbFunctionality)
