9a8f24f487ab18a4d8479623adafe80b930a3943
Suraj Raj <srjdev2003@gmail.com>
2025-02-25 15:06:30 +0530

    adding some js for background

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
  * Write your challenge solution here
  */
-
+const body = document.body;
 const toggleBtn = document.getElementById('toggleButton')
 // console.log(toggleBtn.innerText)
 const bulb = document.getElementById('bulb')
@@ -14,13 +14,15 @@ function bulbFunctionality(){
         // it means bulb glow
 
         //this line basically define we have add .off class properties inside the bulb dinamically
-        bulb.classList.remove('off')   
+        bulb.classList.remove('off') 
+        body.classList.add('dark-mode');  
         toggleBtn.innerText='Turn Off';
         bulbStatus.innerText = 'Status: On'
 
     } else{
         // it means bulb not glow
         bulb.classList.add('off')
+        body.classList.remove('dark-mode');
         toggleBtn.innerText='Turn On';  
         bulbStatus.innerText = 'Status: Off';
     }
